98c649070a126bbfa634641a0e9daf9719c6534f
Daniel Penn <danielpenn100@gmail.com>
2026-09-14 17:24:23 +0300

    Implement Listener class

---
+++ listener.py
@@ -0,0 +1,31 @@
+import socket
+
+from connection import Connection
+
+
+class Listener:
+    def __init__(self, port, host, backlog=1000):
+        self.host = host
+        self.port = port
+        self.backlog = backlog
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(port={self.port}, host={self.host}, backlog={self.backlog})"
+
+    def start(self):
+        self.connection.bind((self.host, self.port))
+        self.connection.listen(self.backlog)
+
+    def stop(self):
+        self.connection.close()
+
+    def accept(self):
+        connection, _ = self.connection.accept()
+        return Connection(connection)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        self.stop()
