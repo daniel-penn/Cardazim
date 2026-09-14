d73652037b7bfe478cb312f9e32fb6cc6819a7e1
Daniel Penn <danielpenn100@gmail.com>
2026-09-14 15:11:28 +0300

    Add threading to run_server

--- server.py
+++ server.py
@@ -1,11 +1,26 @@
 import argparse
 import socket
+import threading
 
 
 def run_server(ip, port):
     """
     Blocking process that listens to a port and IP and prints all data received.
     """
+    lock = threading.Lock()
+
+    def handle_connection(connection):
+        with connection:
+            chunks = []
+            while True:
+                data = connection.recv(4096)
+                if not data:
+                    break
+                chunks.append(data)
+            received_data = b"".join(chunks)
+            with lock:
+                print(f"Received data: {received_data.decode('utf-8')}")
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.bind((ip, port))
         sock.listen()
@@ -16,24 +31,16 @@ def run_server(ip, port):
                 connection, _ = sock.accept()
             except socket.timeout:
                 continue  # Loop, allowing keyboard interrupts to register.
-            chunks = []
-            while True:
-                data = connection.recv(4096)
-                if not data:
-                    break
-                chunks.append(data)
-            received_data = b"".join(chunks)
-            print(f"Received data: {received_data.decode('utf-8')}")
-            connection.close()
+            threading.Thread(
+                target=handle_connection, args=(connection,), daemon=True
+            ).start()
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Launch a server.")
-    
-    parser.add_argument("server_ip", type=str,
-                         help="the server's listening ip")
-    parser.add_argument("server_port", type=int,
-                         help="the server's listening port")
+
+    parser.add_argument("server_ip", type=str, help="the server's listening ip")
+    parser.add_argument("server_port", type=int, help="the server's listening port")
     return parser.parse_args()
 
 
@@ -42,7 +49,7 @@ def main():
     run_server(args.server_ip, args.server_port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
